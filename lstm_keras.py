4d9375b01c2ca0989fcb3ba05eee4f380f61f473
Sidharth Verma <sidhartv@andrew.cmu.edu>
2019-05-06 16:51:15 -0400

    Added function for single lock training

--- lstm_keras.py
+++ lstm_keras.py
@@ -66,10 +66,47 @@ def get_model_dimensions(log_file):
 
     return num_locks
 
+
+
+def get_combos(trace_file):
+	combos = set()
+	with open(trace_file, 'r') as fp:
+		for line in fp:
+			thread_time, lock_id, event_type = parse_line(line)
+			combos.add((lock_id, event_type))
+	return list(combos)
+
+
+def train(x, y, lr, batch_size, epochs, lock_id, event_id, file_prefix, verbose=False):
+	print('[INFO] Started process for ' + file_prefix + ' (lock ' + str(lock_id) + ', event ' + str(event_id) + '), ' + str(len(y)) + ' records')
+	model = construct_model(lr)
+
+	x = x.reshape((x.shape[0], 1, x.shape[1]))
+	y = y.reshape((y.shape[0], y.shape[1]))
+
+	if verbose:
+		hist = model.fit(x, y, epochs=epochs, batch_size=batch_size, verbose=1)
+	else:
+		hist = model.fit(x, y, epochs=epochs, batch_size=batch_size, verbose=0)
+	filename = file_prefix + '_' + str(lock_id) + '_' + str(event_id) + '.h5'
+	model.save(filename)
+	print('[INFO] Saved model to ' + filename)
+
+def train_single(trace_file, lock_id, event_id, batch_size, epochs, lr, output_file_prefix):
+	combo_x, combo_y = parse_trace(trace_file, lock_id, event_id)
+	if len(combo_x) == 0:
+		return
+	train(combo_x, combo_y, lr, batch_size, epochs, lock_id, event_id, output_file_prefix, verbose=True)
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--trace-dir', dest='input_dir', type=str,
                         default=None, help="Filepath of log to build model of")
+    parser.add_argument('--trace-file', dest='trace_file', type=str, default=None)
+    parser.add_argument('--lock-id', dest=lock_id, type=int, default=-1)
+    parser.add_argument('--event-id', dest=event_id, type=int, default=-1)
+
     parser.add_argument('--model-dir', dest='output_dir', type=str,
                         default=None, help="Filepath of log to build model of")
     parser.add_argument('--lr', dest='lr', type=float,
@@ -81,32 +118,16 @@ def parse_args():
 
     return parser.parse_args()
 
-def get_combos(trace_file):
-	combos = set()
-	with open(trace_file, 'r') as fp:
-		for line in fp:
-			thread_time, lock_id, event_type = parse_line(line)
-			combos.add((lock_id, event_type))
-	return list(combos)
-
-
-def train(x, y, lr, batch_size, epochs, lock_id, event_id, file_prefix):
-	print('[INFO] Started process for ' + file_prefix + ' (lock ' + str(lock_id) + ', event ' + str(event_id) + '), ' + str(len(y)) + ' records')
-	model = construct_model(lr)
-
-	x = x.reshape((x.shape[0], 1, x.shape[1]))
-	y = y.reshape((y.shape[0], y.shape[1]))
-
-	hist = model.fit(x, y, epochs=epochs, batch_size=batch_size, verbose=0)
-	filename = file_prefix + '_' + str(lock_id) + '_' + str(event_id) + '.h5'
-	model.save(filename)
-	print('[INFO] Saved model to ' + filename)
-
 def main():
 	args = parse_args()
 	lr = args.lr
 	epochs = args.epochs
 	batch_size = args.batch_size
+	output_file_prefix = args.output_dir + '/' + file[:-6]
+	if args.trace_file != None:
+		train_single(trace_file, args.lock_id, args.event_id, batch_size, epochs, lr, output_file_prefix)
+		return
+
 	procs = []
 
 	files = os.listdir(args.input_dir)
