44b7016013a048826dd819660c477266ee78cf18
Ubuntu <ubuntu@ip-172-31-29-150.us-west-2.compute.internal>
2019-05-07 23:13:37 +0000

    Place error file

--- test_trace.py
+++ test_trace.py
@@ -1,4 +1,6 @@
 import keras
+from tqdm import tqdm
+import numpy as np
 import argparse
 import tensorflow as tf
 
@@ -9,7 +11,7 @@ def parse_args():
     parser.add_argument('--event-id', dest='event_id', type=int, default=-1)
     parser.add_argument('--model-file', dest='model_file', type=str,
                         default=None, help="Filepath of existing model")
-
+    parser.add_argument('--error-file', dest='error_file', type=str, default=None, help='Filepath to save the error array')
     return parser.parse_args()
 
 
@@ -31,6 +33,7 @@ def parse_trace(trace_file, filter_lock_id, filter_event):
 	with open(trace_file, 'r') as fp:
 		for line in fp:
 			thread_time, lock_id, event_type = parse_line(line)
+			#print(lock_id)
 			if lock_id != filter_lock_id or event_type != filter_event:
 				continue
 			delta = thread_time - last_thread_time
@@ -39,24 +42,32 @@ def parse_trace(trace_file, filter_lock_id, filter_event):
 			rec = np.array(delta).reshape(1,1)
 
 			trace.append(rec)
-	if len(trace) < threshold:
-		return [], []
 	x = np.array(trace[:-1])
 	y = np.array(trace[1:])
-
 	return x, y
 
 
 def main():
 	args = parse_args()
 	trace_file = args.trace_file
-	lock_id = args.lock_id
-	event_id = args.event_id
+	lock_id = int(args.lock_id, 16)
+	event_id = int(args.event_id)
 	model_file = args.model_file
 
-	x, y = parse_trace(trace_file, lock_id, event_id)
+	fullx, fully = parse_trace(trace_file, lock_id, event_id)
+	losses = []	
 	model = keras.models.load_model(model_file)
+	for i in tqdm(range(len(fully))):
+		x = fullx[i:i+1]
+		y = fully[i:i+1]
+		x = x.reshape((x.shape[0], 1, x.shape[1]))
+		y = y.reshape((y.shape[0], y.shape[1]))
 
-	model.evaluate(x,y)
+		losses.append(model.evaluate(x,y, verbose=0))
+
+	losses = np.array(losses)
+	err = np.sqrt(losses)
+
+	np.save(args.error_file, err)
 
 main()
