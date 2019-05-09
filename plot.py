1d85e6ba0bc01b614e481b71fad3e8eb129c42c2
Sidharth Verma <sidhartv@andrew.cmu.edu>
2019-05-08 23:29:06 -0400

    Plot with variable y

--- plot.py
+++ plot.py
@@ -3,21 +3,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 from glob import glob
 from tqdm import tqdm
-def plot_figure(errs, sample_nums, plot_filename):
+def plot_figure(errs, sample_nums, plot_filename, ylim):
 	median = errs[-1]
-	errs = errs[:-1]
+	err = errs[:-1]
 	plt.figure()
+	if len(sample_nums) > len(err):
+		sample_nums = sample_nums[:-1]
+
 	plt.plot(sample_nums, err)
 
 	avg = np.mean(err)
 	p99 = np.percentile(err, 90)
-	plt.axhline(p99, color='k', linestyle='dashed')
-	plt.ylim((0,20))
+	plt.axhline(median, color='k', linestyle='dashed', label='median')
+	if ylim > 0:
+		plt.ylim((0,ylim))
+	else:
+		plt.ylim(bottom=0)
 
 	_, max_x = plt.xlim()
 	_, max_y = plt.ylim()
 
-	plt.text(max_x - max_x/7, max_y + max_y/15,
+	plt.text(10, max_y,
                 'p90: {:.2f}\n median delta-t: {}'.format(p99, median))
 	plt.xlabel('Progression of trace')
 	plt.ylabel('Error in microseconds')
@@ -26,6 +32,7 @@ def plot_figure(errs, sample_nums, plot_filename):
 
 def main():
 	directory = argv[1]
+	ylim = int(argv[2])
 	if directory[-1] != '/':
 		directory += '/'
 	files = glob(directory + '*.npz')
@@ -35,6 +42,6 @@ def main():
 		npfile = np.load(file)
 		errs = npfile['err']
 		sample_nums = npfile['sample_num']
-		plot_figure(errs, sample_nums, plot_filename)
+		plot_figure(errs, sample_nums, plot_filename, ylim)
 
 main()
